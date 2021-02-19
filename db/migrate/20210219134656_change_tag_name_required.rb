27d5351d5aafa9eca30af7329d513ebe100a9473
lmbernardo7520112 <lmbernardo752011@gmail.com>
2021-02-19 11:00:54 -0300

    Bug - Adiciona validação de obrigratoriedade e unicidade em tag name

---
+++ db/migrate/20210219134656_change_tag_name_required.rb
@@ -0,0 +1,6 @@
+class ChangeTagNameRequired < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :tags, :name, false
+    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
+  end
+end
